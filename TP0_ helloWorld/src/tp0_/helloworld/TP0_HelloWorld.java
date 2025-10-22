ec1a4565f1f2dd7186157eabcaec3902f6194c9d
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-10-22 16:10:16 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,23 @@
+/*HelloWorld
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/*role
+ */
+public class TP0_HelloWorld {
+
+    /*20/10/2025
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ;
+        int i = 20;
+        String prenom;
+        Scanner sc ;
+        sc = new Scanner (System.in) ;
+        System.out.println("Quel est votre prénom ?");
+        prenom = sc.nextLine(); 
+ }
+    
+}
