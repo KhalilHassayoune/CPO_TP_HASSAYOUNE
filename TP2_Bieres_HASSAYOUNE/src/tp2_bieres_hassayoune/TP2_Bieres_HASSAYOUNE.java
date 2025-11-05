afcd51bab22b6039820fb31f91121655af8ee03a
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-11-05 10:27:23 +0100

    Création d'un nouveau projet versionné

--- TP2_Bieres_HASSAYOUNE/src/tp2_bieres_hassayoune/TP2_Bieres_HASSAYOUNE.java
+++ TP2_Bieres_HASSAYOUNE/src/tp2_bieres_hassayoune/TP2_Bieres_HASSAYOUNE.java
@@ -14,14 +14,40 @@ public class TP2_Bieres_HASSAYOUNE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls",
+7.0 ,"Dubuisson") ;
 
 uneBiere.Nom = "Cuve des trolls";
 uneBiere.degreAlcool = 7.0 ;
-BouteilleBiere brasserie = new BouteilleBiere() ;
-brasserie.Nom = "Dubuisson";
-brasserie.ouverte=false  ;
+String brasserie;
+uneBiere.brasserie="Dubuisson";
+boolean ouverte;
+uneBiere.ouverte=false  ;
 uneBiere.lireEtiquette();
+BouteilleBiere secondeBiere = new BouteilleBiere("Leffe",
+6.6 ,"Abbaye de Leffe") ;
+
+secondeBiere.Nom = " Leffe";
+secondeBiere.degreAlcool = 6.6 ;
+
+secondeBiere.brasserie="Abbaye de Leffe";
+secondeBiere.lireEtiquette();
+BouteilleBiere troisiemeBiere = new BouteilleBiere("corona",
+10.0 ,"Abbaye de corona") ;
+troisiemeBiere.lireEtiquette();
+troisiemeBiere.Décapsuler();
+BouteilleBiere nBiere = new BouteilleBiere("corona",
+10.0 ,"Abbaye de corona") ;
+nBiere.lireEtiquette();
+nBiere.Décapsuler();
+BouteilleBiere niemeBiere = new BouteilleBiere("corona",
+10.0 ,"Abbaye de corona") ;
+niemeBiere.lireEtiquette();
+niemeBiere.Décapsuler();
+System.out.println(uneBiere) ;
+System.out.println(niemeBiere);
+System.out.println(nBiere);
+
     }
     
 }
