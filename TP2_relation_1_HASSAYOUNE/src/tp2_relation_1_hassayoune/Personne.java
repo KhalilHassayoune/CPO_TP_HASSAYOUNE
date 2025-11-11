d27c1c0cf41259018686ae71c60591b4fed88820
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-11-12 00:28:23 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_relation_1_HASSAYOUNE/src/tp2_relation_1_hassayoune/Personne.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_hassayoune;
+
+/**
+ *
+ * @author khalo
+ */
+public class Personne {
+    String nom;
+    String prenom;
+     public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+     int nbVoitures=0 ;
+Voiture [] liste_voitures= new Voiture [3] ;
+
+    
+}
