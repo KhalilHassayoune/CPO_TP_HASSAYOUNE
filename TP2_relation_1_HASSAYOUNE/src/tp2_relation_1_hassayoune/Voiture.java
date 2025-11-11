646625b0364c31c0562505156078172399bad924
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-11-12 00:28:05 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_relation_1_HASSAYOUNE/src/tp2_relation_1_hassayoune/Voiture.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_hassayoune;
+
+/**
+ *
+ * @author khalo
+ */
+public class Voiture {
+    String Modele ;
+    String Marque ;
+    int PuissanceCV ;
+    public Voiture(String Modele, String Marque, int PuissanceCV) {
+        this.Modele = Modele;
+        this.Marque = Marque;
+        this.PuissanceCV = PuissanceCV;
+    }
+    public String toString (){
+    return "nb de conversions";}
+    Personne Proprietaire =null ;
+    
+    
+}
