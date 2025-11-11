9180ae785dfe928143c79a9184ded792b90f5f8e
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-11-11 23:52:49 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_manip_HASSAYOUNE/src/tp2_manip_hassayoune/Moussaka.java
@@ -0,0 +1,17 @@
+package tp2_manip_hassayoune;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author khalo
+ */
+public class Moussaka {
+    int nbCalories ;
+    public Moussaka(int calories) {
+        nbCalories = calories;
+    }
+}
