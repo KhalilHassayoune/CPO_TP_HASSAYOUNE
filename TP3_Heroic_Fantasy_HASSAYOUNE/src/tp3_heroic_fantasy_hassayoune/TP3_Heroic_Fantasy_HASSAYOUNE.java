f241fe806c94f913fd09d3964ace2ae17bd81fbc
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-11-18 15:30:26 +0100

    Création d'un nouveau projet versionné

---
+++ TP3_Heroic_Fantasy_HASSAYOUNE/src/tp3_heroic_fantasy_hassayoune/TP3_Heroic_Fantasy_HASSAYOUNE.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_hassayoune;
+
+/**
+ *
+ * @author khalo
+ */
+public class TP3_Heroic_Fantasy_HASSAYOUNE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Arme pistolet =new Arme("pistolet",100);
+       Epee premier =new Epee("Excalibur",7,5);
+       Epee deuxieme =new Epee("Durandal",4,7);
+       Baton first =new Baton("Chêne", 4, 5);
+       Baton second =new Baton("Charme", 5, 6);
+       
+       
+            
+            
+        }
+        // TODO code application logic here
+    }
+    
+
