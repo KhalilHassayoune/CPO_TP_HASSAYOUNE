948a7403bed4fb21ff46f89d03dbd716d9e6da94
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-12-14 12:20:41 +0100

    Création des méthodes

--- miniprojet_cadenas_HASSAYOUNE/src/Combinaison.java
+++ miniprojet_cadenas_HASSAYOUNE/src/Combinaison.java
@@ -8,8 +8,61 @@
  * @author khalo
  */
 public class Combinaison {
+    private int[] combiSecrete = new int[4];
+    private int[] combiTestee = new int[4];
+    
+    
+public void genererCombiAleat() {
+    
+       
+      
+    for (int i = 0; i < 4; i++) {
+        combiSecrete[i] = (int)(Math.random() * 10);
+    }
+}
+
+
+
+
+
+public int[] comparerCombi() {
+    int nbExact=0;
+    int nbTropHaut=0;
+    int nbTropBas=0;
+    for (int i=0;i<4;i++){
+        if (combiTestee[i] == combiSecrete[i]) 
+            nbExact++;
+         
+        if (combiSecrete[i] > combiTestee[i]) 
+            nbTropBas++;
+        
+// contenu ici
+if (combiTestee[i] > combiSecrete[i]) 
+            nbTropHaut++;
+    }
+        
+return new int[]{nbExact, nbTropHaut,nbTropBas};
+}
+public void augmenterChiffre(int indice) {
+     combiTestee[indice]++;
+    if (combiTestee[indice] == 10) {
+        combiTestee[indice] = 0;
+    }
+}
+public int[] getCombiTestee() {
+    return combiTestee;
+}
+public void diminuerChiffre(int indice) {
+    combiTestee[indice]--;
+    if (combiTestee[indice] == -1) {
+        combiTestee[indice] = 9;
+    }
+
+
+}}
+    
     
    
 
-}
+
 
