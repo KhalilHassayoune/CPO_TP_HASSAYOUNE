f621a83a0acc8a86120625e799e2bc6d32d0c18f
Khalil Hassayoune <khalil.hassayoune@epfedu.fr>
2025-12-19 10:03:46 +0100

    Ajout des fichiers manquants

---
+++ TP3_Heroic_Fantasy_HASSAYOUNE/src/Armes/Magicien.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author khalo
+ */
+class Magicien {
+
+    Magicien(String gandalf, int par, int par1, boolean par2) {
+    }
+    
+}
